8dd16a0a7680e198848f75e853ac94377ea50856
Amazingkenneth <81886982+Amazingkenneth@users.noreply.github.com>
2022-07-27 09:58:32 +0000

    update

--- admin/gitalk.js
+++ admin/gitalk.js
@@ -1,10 +1,11 @@
+var title = location.pathname.substr(0, 50);
 var gitalk = new Gitalk({
   clientID: '24c5d5ae0387e551cf41',
   clientSecret: 'f2999b8c00c3c20d5c6213aa34d22fcad5c3edd4',
   repo: 'amazingkenneth.github.io',
   owner: 'Amazingkenneth',
   admin: ['Amazingkenneth'],
-  id: location.pathname,
+  id: title,
   perPage: 10,
   createIssueManually: true,
   enableHotKey: true,
