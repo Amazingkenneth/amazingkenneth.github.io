cd26cb1d8fb3341583733472fab6103080d74093
Amazingkenneth <81886982+Amazingkenneth@users.noreply.github.com>
2022-07-28 08:44:24 +0000

    update

--- admin/work.js
+++ admin/work.js
@@ -26,7 +26,9 @@ if (pageid == null) {
   pageid = location.pathname.substring(0, 49);
 }
 pageid = decodeURI(pageid);
-
+var cnissue = "欢迎来到此页面留言、评论、提出问题，这里的评论与网页底部上的 Gitalk 评论区中的留言互相可见。";
+var enissue = "Comments and questions are welcome on this page. Comments here can be seen in the Gitalk section at the bottom of the corresponding page (URI)."
+var issuebody = "URL: " + location.href + '\n' + cnissue + '\n' + enissue;
 var gitalk = new Gitalk({
   clientID: '24c5d5ae0387e551cf41',
   clientSecret: 'f2999b8c00c3c20d5c6213aa34d22fcad5c3edd4',
@@ -35,7 +37,8 @@ var gitalk = new Gitalk({
   admin: ['Amazingkenneth'],
   id: pageid,
   title: Title,
-  perPage: 10,
+  body: issuebody,
+  perPage: 5,
   createIssueManually: true,
   enableHotKey: true,
   distractionFreeMode: true
